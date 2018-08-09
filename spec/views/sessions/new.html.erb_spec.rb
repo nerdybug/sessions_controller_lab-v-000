845a85a8b8dbee5069ba3ce26354743416e1faac
Meredith Reece <mardebugger@gmail.com>
2018-08-09 01:49:00 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe "sessions/new.html.erb", type: :feature do
   scenario "has a form to enter name and a login button" do
     visit "/login"
     expect(page).to have_selector(:link_or_button, 'login')
-    expect(page).to has_element(:form)
+    expect(page).to find_field(:name)
   end
 end
