58537cf7d7d6d9e6944bec889eec7b30c36ba357
Meredith Reece <mardebugger@gmail.com>
2018-08-09 01:28:41 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "sessions/new.html.erb", type: :feature do
-  scenario "User arrives at login page" do
+  scenario "Login view has a login button" do
     visit "/login"
     expect(page).to have_selector(:link_or_button, 'login')
   end
