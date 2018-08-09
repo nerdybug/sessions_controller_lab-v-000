1cb9a142611481c8626faa82cd58df5d4eb3a087
Meredith Reece <mardebugger@gmail.com>
2018-08-09 01:36:55 +0000

    automatically backed up by learn

--- spec/views/application/hello.html.erb_spec.rb
+++ spec/views/application/hello.html.erb_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "application/hello.html.erb", type: :feature do
   scenario "root view greets the logged-in user" do
+    test_name = "We are testing here"
+    post :create, name: test_name
     visit "/"
     expect(page).to have_text('Hi,')
   end
