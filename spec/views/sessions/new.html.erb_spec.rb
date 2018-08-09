57b391ef02ceba2a0d687ef3324e404d2f0800a7
Meredith Reece <mardebugger@gmail.com>
2018-08-09 01:51:19 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "sessions/new.html.erb", type: :feature do
   scenario "has a form to enter name and a login button" do
-    visit "/login"
+    visit '/login'
     expect(page).to have_selector(:link_or_button, 'login')
     expect(page).to have_field(:name)
+  end
+
+  scenario "returns to login if user tries logging in without name" do
+    visit
   end
 end
