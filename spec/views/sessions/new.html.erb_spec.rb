9d68ea2f168c6efa3e0e8fe090768c2ddd43c245
Meredith Reece <mardebugger@gmail.com>
2018-08-09 01:06:25 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "sessions/new.html.erb", type: :feature do
   scenario "User arrives at root page" do
     visit '/'
-    expect(page).to_have_text("login")
+    expect(page).to have_text("login")
   end
 end
